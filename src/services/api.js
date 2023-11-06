9f5bc0b281c99b6e01884d22d7f0b43e6e4e0a08
Leandro <leandro.bernardes@hotmail.com>
2023-11-06 12:52:21 -0300

    Mudança endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-
+const DEV = 'http://127.0.0.1:9000/';
+const PRD = 'https://invoicer-api.vercel.app';
 const api = axios.create({
-  baseURL: "http://127.0.0.1:9000/",
+  baseURL: PRD,
 });
-// https://invoicer-api.vercel.app
+
 export default api;
