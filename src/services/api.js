1075571bd8b9e07b0bd7bafa9fb51091edb6b714
Leandro <leandro.bernardes@hotmail.com>
2023-11-06 12:53:28 -0300

    Comentando api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const DEV = 'http://127.0.0.1:9000/';
+// const DEV = 'http://127.0.0.1:9000/';
 const PRD = 'https://invoicer-api.vercel.app';
 const api = axios.create({
   baseURL: PRD,
